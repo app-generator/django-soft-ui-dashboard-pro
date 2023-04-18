236482f53321c5eadb9cd4616d6ab5975f581616
mominur-helios <mominur.rahman@heliosinsider.com>
2023-04-18 11:10:32 +0600

    In wizard make send data as post

--- home/views.py
+++ home/views.py
@@ -7,3 +7,13 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/dashboards/default.html')
+
+def wizard(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        # ...
+        # here you can send all the information as a post request.
+        print(first_name, last_name, email)
+    return render(request, 'pages/applications/wizard.html')
